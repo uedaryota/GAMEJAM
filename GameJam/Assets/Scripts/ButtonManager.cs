e853c3ee2f81d16e621cbe2c4a82b15fb62d630d
uedaryota <mr.335186@gmail.com>
2021-09-16 15:51:23 +0900

    シーン遷移

---
+++ GameJam/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    GameObject scenetransition;
+    SceneTransition scene;
+    private void Start()
+    {
+        scenetransition = GameObject.Find("SceneTransition");
+        scene = scenetransition.GetComponent<SceneTransition>();
+    }
+    public void OnClickStartButton()
+    {
+        if(name == "StartButton")
+        {
+            scene.GameSceneTransition();
+        }
+        if(name == "TitleButton")
+        {
+            scene.TitleSceneTransition();
+        }
+        if(name == "GameEndButton")
+        {
+            scene.Quit();
+        }
+    }
+}
---
+++ GameJam/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public void TitleSceneTransition()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+    public void GameSceneTransition()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void ResultTransition()
+    {
+        SceneManager.LoadScene("ResultScene");
+    }
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      UnityEngine.Application.Quit();
+#endif
+    }
+}
