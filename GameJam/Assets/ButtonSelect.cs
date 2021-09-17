f5379b9dd4465891b01cd445a8d8c484608123aa
uedaryota <mr.335186@gmail.com>
2021-09-17 15:10:56 +0900

    タイトルシーンとリザルトシーンのUI整理とキーボード対応

---
+++ GameJam/Assets/ButtonSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSelect : MonoBehaviour
+{
+    private Button FirstSelectButton;
+
+    void Start()
+    {
+        if (name == "TitlePanel")
+        {
+            FirstSelectButton = GameObject.Find("StartButton").GetComponent<Button>();
+        }
+        if(name == "ResultPanel")
+        {
+            FirstSelectButton = GameObject.Find("TitleButton").GetComponent<Button>();
+        }
+        FirstSelectButton.Select();
+    }
+}
