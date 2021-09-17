42c773bf510b3e95e334c306c1ffc726d0189e48
OverdrIV_1203 <sakai1203yuu@gmail.com>
2021-09-17 13:19:18 +0900

    add.今の作業をとりあえず

--- GameJam/Assets/Scripts/Arrow.cs
+++ GameJam/Assets/Scripts/Arrow.cs
@@ -5,6 +5,23 @@ namespace Assets.Scripts
 {
     public class Arrow : Wood
     {
-        
+        private float moveSpeed = 4;
+
+        public Arrow(Vector3 pos)
+        {
+            Model = (GameObject)Resources.Load("Prefabs/Arrow");
+            Create(pos);
+        }
+
+        protected override void AppendUpdate()
+        {
+            Move();
+            base.AppendUpdate();
+        }
+
+        private void Move()
+        {
+            Model.transform.position += Model.transform.forward * moveSpeed * Time.deltaTime;
+        }
     }
 }
