b09a98989582b2960b6ad7bfc631412b854c2d53
masamx2 <masatakamx2@gmail.com>
2021-09-18 13:01:52 +0900

    横移動

--- GameJam/Assets/Scripts/Target.cs
+++ GameJam/Assets/Scripts/Target.cs
@@ -5,15 +5,21 @@ namespace Assets.Scripts
 {
     public class Target : Wood
     {
+        private float move = 0.01f;
+        private bool moveFlag = false;
         public Target(Vector3 pos)
         {
             Model = (GameObject)Resources.Load("Prefabs/Target");
             Create(pos);
         }
-      
+        protected override void AppendInitialize()
+        {
+            moveFlag = false;
+        }
         protected override void AppendUpdate()
         {
             Hit();
+            Move();
             base.AppendUpdate();
         }
 
@@ -24,6 +30,28 @@ namespace Assets.Scripts
             if(Model.transform.GetComponent<CheckHit>().HitFlag)
             {
                 DeadFlag = true;
+            }
+        }
+        private void Move()
+        {
+            if(Model.transform.position.x>=3.9f)
+            {
+                moveFlag = true;
+                Debug.Log("aa");
+            }
+            
+            if(Model.transform.position.x<=-3.9f)
+            {
+                moveFlag = false;
+                Debug.Log("bb");
+            }
+            if(moveFlag == true)
+            {
+                Model.transform.Translate(-move, 0, 0);
+            }
+            if (moveFlag == false)
+            {
+                Model.transform.Translate(move, 0, 0);
             }
         }
     }
