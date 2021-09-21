4952ec9ebe519a1174ebb64c9ca11d49e0613b6c
masamx2 <masatakamx2@gmail.com>
2021-09-21 14:44:54 +0900

    スコア合計とエンドシーン編集中

--- GameJam/Assets/Scripts/ScoreDisplay.cs
+++ GameJam/Assets/Scripts/ScoreDisplay.cs
@@ -5,20 +5,34 @@ using UnityEngine.UI;
 
 public class ScoreDisplay : MonoBehaviour
 {
+    private GameScore gameScore;
     private Text text;
     private int rank1, rank2, rank3;
-
+    private int first =1000, second =500, third = 100;
     void Start()
     {
-        text = GameObject.Find("ScoreText").GetComponent<Text>();
-
-        text.text = "1:" + rank1 + "\n" + "2:" + rank2 + "\n" + "3:" + rank3;
+        
+        Dictionary<string, int> dictionary = new Dictionary<string, int>()
+        {
+            { "1位", first },
+            { "2位", second },
+            { "3位", third },
+        };
+        foreach(KeyValuePair<string,int>pair in dictionary)
+        {
+            string rank = pair.Key;
+            int rankNumber = pair.Value;
+            Debug.Log(rank + ":" + rankNumber);
+        }
     }
-
-    public void SetRank(int num1, int num2, int num3)
+    public void Update()
     {
-        rank1 = num1;
-        rank2 = num2;
-        rank3 = num3;
+        
+    }
+    private void SaveData()
+    {
+        PlayerPrefs.SetInt("1位", first);
+        PlayerPrefs.SetInt("2位", second);
+        PlayerPrefs.SetInt("3位", third);
     }
 }
